ad82c631a7f40c79b6722bf231f0c109ec04974c
Paulo H Motta <paulomotta@customcode.com.br>
2024-09-27 17:44:52 -0300

    Desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes == null || hospedes.Count == 0)
             {
-                Hospedes = hospedes;
+                throw new ArgumentNullException(nameof(hospedes), "A lista de hóspedes não pode ser nula ou vazia.");
+            }
+
+            if (Suite.Capacidade < hospedes.Count)
+            {
+                throw new Exception("Número de hóspedes é maior que a capacidade da suíte.");
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Atribui a lista de hóspedes se a capacidade permitir
+                Hospedes = hospedes;
             }
         }
 
@@ -35,9 +38,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +46,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.10M;
             }
 
             return valor;
